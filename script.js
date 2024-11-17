94a70ce9e1a0f39753f76931e3a67867d28e4474
sourapple023 <imjohnlee@live.com>
2024-11-16 21:41:12 -0800

    fresh

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 document.getElementById('dark-mode-toggle').addEventListener('click', function() {
     document.body.classList.toggle('dark-mode');
+    let icon = document.getElementById('dark-mode-icon');
+    if (document.body.classList.contains('dark-mode')) {
+        icon.classList.replace('fa-sun', 'fa-moon');
+    } else {
+        icon.classList.replace('fa-moon', 'fa-sun');
+    }
 });
